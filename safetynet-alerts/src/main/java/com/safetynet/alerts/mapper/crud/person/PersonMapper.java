e1a001219ff21d18461642442f95ecc3d2708a00
rafael alcaniz <alcaniz.rst@gmail.com>
2025-09-23 08:28:18 +0200

    mapper

--- safetynet-alerts/src/main/java/com/safetynet/alerts/mapper/crud/person/PersonMapper.java
+++ safetynet-alerts/src/main/java/com/safetynet/alerts/mapper/crud/person/PersonMapper.java
@@ -5,14 +5,19 @@ import com.safetynet.alerts.dto.crud.person.PersonResponseDto;
 import com.safetynet.alerts.dto.crud.person.PersonUpdateDto;
 import com.safetynet.alerts.mapper.CentralMapperConfig;
 import com.safetynet.alerts.model.Person;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = CentralMapperConfig.class)
 public interface PersonMapper {
+
     Person toEntity(PersonCreateDto dto);
-    @BeanMapping
-    void update(@MappingTarget Person target, PersonUpdateDto dto); // PUT complet
+
+    // PUT complet : on n’autorise pas la MAJ de l’identité
+    @Mapping(target = "firstName", ignore = true)
+    @Mapping(target = "lastName",  ignore = true)
+    void update(@MappingTarget Person target, PersonUpdateDto dto);
+
     PersonResponseDto toResponse(Person entity);
 }
