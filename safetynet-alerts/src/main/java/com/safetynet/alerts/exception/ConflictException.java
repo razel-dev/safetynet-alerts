bff3dca5b5f9fb8debc71b7503ef5b3ae4715ec5
rafael alcaniz <alcaniz.rst@gmail.com>
2025-09-23 16:00:04 +0200

    .

---
+++ safetynet-alerts/src/main/java/com/safetynet/alerts/exception/ConflictException.java
@@ -0,0 +1,21 @@
+package com.safetynet.alerts.exception;
+
+public class ConflictException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ConflictException() {
+        super();
+    }
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+    public ConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConflictException(Throwable cause) {
+        super(cause);
+    }
+}
