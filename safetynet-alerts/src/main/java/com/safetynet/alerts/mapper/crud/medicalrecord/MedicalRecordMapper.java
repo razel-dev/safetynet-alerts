852e7b721e36c108a562f5deb15099bc6dfd492d
rafael alcaniz <alcaniz.rst@gmail.com>
2025-09-23 08:29:40 +0200

    mapper

--- safetynet-alerts/src/main/java/com/safetynet/alerts/mapper/crud/medicalrecord/MedicalRecordMapper.java
+++ safetynet-alerts/src/main/java/com/safetynet/alerts/mapper/crud/medicalrecord/MedicalRecordMapper.java
@@ -5,14 +5,19 @@ import com.safetynet.alerts.dto.crud.medicalrecord.MedicalRecordResponseDto;
 import com.safetynet.alerts.dto.crud.medicalrecord.MedicalRecordUpdateDto;
 import com.safetynet.alerts.mapper.CentralMapperConfig;
 import com.safetynet.alerts.model.MedicalRecord;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(config = CentralMapperConfig.class)
 public interface MedicalRecordMapper {
+
     MedicalRecord toEntity(MedicalRecordCreateDto dto);
-    @BeanMapping
-    void update(@MappingTarget MedicalRecord target, MedicalRecordUpdateDto dto); // PUT complet
+
+    // PUT complet : l’identité ne change pas
+    @Mapping(target = "firstName", ignore = true)
+    @Mapping(target = "lastName",  ignore = true)
+    void update(@MappingTarget MedicalRecord target, MedicalRecordUpdateDto dto);
+
     MedicalRecordResponseDto toResponse(MedicalRecord entity);
 }
