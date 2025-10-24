ecd930028e9808f3c136cd7014ea903f37d27493
alcan <alcan@example.com>
2025-10-24 09:52:55 +0200

    tests

--- safetynet-alerts/src/test/java/com/safetynet/alerts/config/DataLoaderTest.java
+++ safetynet-alerts/src/test/java/com/safetynet/alerts/config/DataLoaderTest.java
@@ -17,7 +17,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-// ... existing code ...
 /**
  * Tests unitaires de DataLoader sans démarrer de contexte Spring.
  * Stratégie:
